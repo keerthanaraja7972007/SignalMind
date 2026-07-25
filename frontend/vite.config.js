2536183a6796235961eef4d1a6511cfe96b04169
keerthanaraja7972007 <keerthanaraja7972007@gmail.com>
2026-07-26 00:45:36 +0530

    Add base '/' to vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: '/', // Strictly forces root paths for all built scripts and assets
+  build: {
+    outDir: 'dist',
+  }
 })
